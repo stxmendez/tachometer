8dd5c6f3882753963573e8cd7a121445159a7e42
Miguel Méndez <stxmendez@gmail.com>
2020-11-08 17:49:45 -0500

    Updated to default to emitting data via the serial port. Also works with 16 segment encoder.

--- src/tach/tach.ino
+++ src/tach/tach.ino
@@ -3,55 +3,56 @@
  * on the INT0 pin. For this application that equates to the amount of time required
  * for one revolution of the lathe chuck.
  */
-#include <LiquidCrystal_I2C.h>
-#include "circular_queue.h"
+//#define DEBUG
+//#define LCD
+#define SERIAL_DEBUG
 
-#define DEBUG
-#ifdef DEBUG
-#define MS_ROTATION 600
-#define PD7_PIN   (7)
+#ifdef LCD
+#include <LiquidCrystal_I2C.h>
 #endif
+
+#include "circular_queue.h"
 
 #define INT0_PIN  (2)
 #define LED_PIN   (13)
+
+// 1 minute, 60 second, 1 second 1000 ms, 1 ms 1000 us
+#define MICRO_SECS_PER_MINUTE 60000000UL
+
+// Number of cycles per complete rotation of the encoder. Also equivalent to 
+// the number of encoder segments.
+#define CYCLES_PER_ROTATION   16UL
 
 volatile unsigned long lastCaptureTime = 0;
 volatile unsigned long lastDelta = 0;
 
 volatile CircularQueue isrCircularQueue(8);
 
+#ifdef LCD
 // LiquidCrystal_I2C  set the LCD address to 0x27 for a 20 chars and 4 line display
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+#endif
 
 void setup() {
   Serial.begin(115200);
 
+#ifdef LCD
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("Tachometer");
+#endif //LCD
 
   noInterrupts();
 
-#ifdef DEBUG
-  pinMode(PD7_PIN, OUTPUT);
-  digitalWrite(PD7_PIN, HIGH);
-#endif
-
   pinMode(LED_PIN, OUTPUT);
   pinMode(INT0_PIN, INPUT_PULLUP);
+  
   attachInterrupt(digitalPinToInterrupt(INT0_PIN), captureEvent, FALLING);
   interrupts();
 }
 
 void loop() {
-//#ifdef DEBUG  
-//  digitalWrite(PD7_PIN, LOW);
-//  delay(MS_ROTATION/2);
-//  digitalWrite(PD7_PIN, HIGH);
-//  delay(MS_ROTATION/2);
-//#endif 
-
   unsigned long delta = 0;
   noInterrupts();
   if (lastCaptureTime != 0) {
@@ -67,13 +68,16 @@ void loop() {
   unsigned long rpm = computeRPM(delta);
   char buf[128];
   sprintf(buf, "RPM: %6lu", rpm);
+
+#ifdef LCD  
   lcd.setCursor(0, 3);
   lcd.print(buf);
+#endif LCD
 
-//#ifdef DEBUG 
-//  sprintf(buf, "ts:%lu, delta:%lu, rpm:%lu", lastCaptureTime, delta, rpm);
-//  Serial.println(buf);
-//#endif
+#ifdef SERIAL_DEBUG 
+  sprintf(buf, "ts:%lu, delta:%lu, rpm:%lu", lastCaptureTime, delta, rpm);
+  Serial.println(buf);
+#endif
 
   delay(100);
 }
@@ -87,11 +91,13 @@ void captureEvent() {
 
   lastDelta = computeDelta(now, lastCaptureTime);
   lastCaptureTime = now;
-  
+
+#if defined(DEBUG) && defined(SERIAL_DEBUG)
   Serial.print("now=");
   Serial.print(now);
   Serial.print(" delta=");
   Serial.println(lastDelta);
+#endif
 }
 
 static inline unsigned long computeDelta(unsigned long t0, unsigned long t1) {
@@ -102,11 +108,11 @@ static inline unsigned long computeDelta(unsigned long t0, unsigned long t1) {
   return t0 - t1;
 }
 
+
 static inline unsigned long computeRPM(unsigned long cycleTimeMicros) {
-  // 1 minute, 60 second, 1 second 1000 ms, 1 ms 1000 us
   if (cycleTimeMicros == 0) {
     return 0;
   }
 
-  return 60000000UL / cycleTimeMicros;
+  return MICRO_SECS_PER_MINUTE / (cycleTimeMicros * CYCLES_PER_ROTATION);
 }
